5c207d816f5bc0a20a99b1d8d783dc7fd521b7fb
N-Process <DouglasMBeck@yahoo.com>
2018-11-28 02:09:53 -0700

    Initial commit

---
+++ WorkflowHost/WorkflowHost/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowHost
+{
+    public class Class1
+    {
+    }
+}
